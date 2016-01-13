2f96624dae3f04684df7f2b15a3aed8344180199
Kenneth Lalobo <lalobo@gmail.com>
2016-01-13 00:01:21 +0000

    add command ability

---
+++ src/Mooti/Console/Command/AddRepositoryCommand.php
@@ -0,0 +1,45 @@
+<?php
+namespace Mooti\Console\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class AddRepositoryCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('repository:add')
+            ->setDescription('Add a repository')
+            ->addArgument(
+                'type',
+                InputArgument::REQUIRED,
+                'Either "module", "application" or "infrastrature"'
+            )
+            ->addArgument(
+                'name',
+                InputArgument::REQUIRED,
+                'the name of the repository'
+            )
+            ->addArgument(
+                'url',
+                InputArgument::REQUIRED,
+                'The git url for the repository'
+            )
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $name = $input->getArgument('name');
+        if ($name) {
+            $text = 'Hello '.$name;
+        } else {
+            $text = 'Hello';
+        }
+
+        $output->writeln($text);
+    }
+}
