bf58f9ee36cb29debcbb40f2271ab9a7e7dbaad8
Ken Lalobo <lalobo@gmail.com>
2016-02-05 17:19:21 +0000

    some fixes due to change in file structure

--- src/Command/Repository/UpdateRepositoryCommand.php
+++ src/Command/Repository/UpdateRepositoryCommand.php
@@ -1,5 +1,5 @@
 <?php
-namespace Mooti\Console\Command\Repository;
+namespace Mooti\Base\Core\Command\Repository;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
