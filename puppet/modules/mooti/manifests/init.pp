52977053f9427c975c56e4cbeae65f4c60aab230
Ken Lalobo <lalobo@gmail.com>
2016-07-06 16:04:59 +0100

    updated version of mooti admin

--- puppet/modules/mooti/manifests/init.pp
+++ puppet/modules/mooti/manifests/init.pp
@@ -25,7 +25,7 @@ class mooti {
     }
 
     exec { "install-mooti-admin":
-        command     => "/mooti/platform/puppet/modules/mooti/files/mooti-intsall.sh 0.0.2",
+        command     => "/mooti/platform/puppet/modules/mooti/files/mooti-intsall.sh 0.0.3",
         cwd         => "/opt/mooti/",
         require     => [
             Class['php'],
